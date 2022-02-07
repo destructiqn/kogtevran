24cda035595cd5df7fd5099c50ad8d0341f484ad
klirii <83013244+klirii@users.noreply.github.com>
2022-02-08 02:29:36 +0800

    Entity struct updated: added isPlayer field

--- conn.go
+++ conn.go
@@ -21,6 +21,7 @@ const (
 
 type Entity struct {
 	Location *Location
+	isPlayer bool
 }
 
 type Location struct {
@@ -53,14 +54,15 @@ type WrappedConn struct {
 
 func (w *WrappedConn) initEntity(entityID int32) {
 	w.EntitiesMutex.Lock()
-	w.Entities[entityID] = &Entity{&Location{}}
+	w.Entities[entityID] = &Entity{&Location{}, false}
 	w.EntitiesMutex.Unlock()
 }
 
-func (w *WrappedConn) initPositionedEntity(entityID int32, x, y, z float64) {
+func (w *WrappedConn) initPositionedEntity(entityID int32, x, y, z float64, isPlayer bool) {
 	w.EntitiesMutex.Lock()
 	w.Entities[entityID] = &Entity{
 		Location: &Location{x, y, z},
+		isPlayer: isPlayer,
 	}
 	w.EntitiesMutex.Unlock()
 }
