238bdece5129c782ae3b795d6c6372d40b7c2d4b
RuscalWorld <me@ruscalworld.ru>
2022-03-11 19:13:17 +0300

    Поддержка SSL

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/destructiqn/kogtevran/net"
@@ -25,7 +26,12 @@ func GetRemoteAddr() string {
 
 func main() {
 	go func() {
-		_ = http.ListenAndServe("0.0.0.0:8080", http.HandlerFunc(proxy.WebsocketHandler))
+		if certPath, ok := os.LookupEnv("KV_CERT_PATH"); ok {
+			keyPath := os.Getenv("KV_CERT_KEY_PATH")
+			_ = http.ListenAndServeTLS("0.0.0.0:8080", certPath, keyPath, http.HandlerFunc(proxy.WebsocketHandler))
+		} else {
+			_ = http.ListenAndServe("0.0.0.0:8080", http.HandlerFunc(proxy.WebsocketHandler))
+		}
 	}()
 
 	proxyServer, err := net.ListenMC("0.0.0.0:25565")
