a178cbba4f3f1e4cfaa4c4538dc0b240aded6abf
RuscalWorld <me@ruscalworld.ru>
2022-03-16 23:19:50 +0300

    Обновлённый интерфейс

---
+++ modules/category.go
@@ -0,0 +1,49 @@
+package modules
+
+const (
+	CategoryCombat   = "Combat"
+	CategoryMovement = "Movement"
+	CategoryPlayer   = "Player"
+	CategoryWorld    = "World"
+	CategoryVisual   = "Visual"
+	CategoryMisc     = "Miscellaneous"
+)
+
+var Categories = map[string][]string{
+	CategoryCombat:   {ModuleAntiKnockback, ModuleKillAura, ModuleMobAura, ModuleTPAura},
+	CategoryMovement: {ModuleFlight, ModuleLongJump},
+	CategoryPlayer:   {ModuleNoFall, ModuleChestStealer, ModuleNoBadEffects, ModuleUnlimitedCPS},
+	CategoryWorld:    {ModuleFastBreak, ModuleNuker},
+	CategoryVisual:   {ModulePlayerESP},
+	CategoryMisc:     {ModuleSpammer, ModuleCMDCam},
+}
+
+type CategoryList []Category
+
+func (c CategoryList) Len() int {
+	return len([]Category(c))
+}
+
+func (c CategoryList) Less(i, j int) bool {
+	return len(c[i].ModuleIDs) < len(c[j].ModuleIDs)
+}
+
+func (c CategoryList) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+type Category struct {
+	Name      string
+	ModuleIDs []string
+}
+
+func GetCategoryList() CategoryList {
+	list := make(CategoryList, 0)
+	for id, modules := range Categories {
+		list = append(list, Category{
+			Name:      id,
+			ModuleIDs: modules,
+		})
+	}
+	return list
+}
