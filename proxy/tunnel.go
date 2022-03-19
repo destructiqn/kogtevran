ca5fe4b5f1f2a31aad11b46fdf83b4b9b419449e
RuscalWorld <me@ruscalworld.ru>
2022-03-19 19:10:42 +0300

    Отдельный файл для связей соединений

--- proxy/tunnel.go
+++ proxy/tunnel.go
@@ -15,59 +15,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-type TunnelPair struct {
-	SessionID string
-	Auxiliary *AuxiliaryChannel
-	Primary   *MinecraftTunnel
-	License   license.License
-}
-
-type TunnelPairID struct {
-	Username   string
-	RemoteAddr string
-}
-
-var CurrentTunnelPool = &TunnelPool{
-	pool: make(map[TunnelPairID]*TunnelPair),
-}
-
-type TunnelPool struct {
-	pool map[TunnelPairID]*TunnelPair
-	sync.Mutex
-}
-
-func (p *TunnelPool) RegisterPair(id TunnelPairID, pair *TunnelPair) {
-	p.Lock()
-	p.pool[id] = pair
-	p.Unlock()
-	UpdateConnectionMetrics()
-}
-
-func (p *TunnelPool) UnregisterPair(id TunnelPairID) {
-	pair, ok := p.GetPair(id)
-	if !ok {
-		return
-	}
-
-	p.Lock()
-	delete(p.pool, id)
-	p.Unlock()
-
-	pair.SessionID = ""
-	_ = pair.Auxiliary.Close()
-	pair.Auxiliary = nil
-
-	pair.Primary.Close()
-	pair.Primary = nil
-
-	UpdateConnectionMetrics()
-}
-
-func (p *TunnelPool) GetPair(id TunnelPairID) (*TunnelPair, bool) {
-	tunnel, ok := p.pool[id]
-	return tunnel, ok
-}
-
 type MinecraftTunnel struct {
 	PairID     TunnelPairID
 	TunnelPair *TunnelPair
@@ -165,56 +112,6 @@ func (t *MinecraftTunnel) Close() {
 	_ = t.Server.Close()
 	_ = t.Client.Close()
 	CurrentTunnelPool.UnregisterPair(t.PairID)
-}
-
-func UpdateConnectionMetrics() {
-	CurrentTunnelPool.Lock()
-	defer CurrentTunnelPool.Unlock()
-
-	var (
-		auxiliaryConnections int
-		minecraftConnections int
-	)
-
-	for _, pair := range CurrentTunnelPool.pool {
-		if pair.Primary != nil {
-			minecraftConnections++
-		}
-
-		if pair.Auxiliary != nil {
-			auxiliaryConnections++
-		}
-	}
-
-	metrics.TotalConnections.With(prometheus.Labels{"type": "auxiliary"}).Set(float64(auxiliaryConnections))
-	metrics.TotalConnections.With(prometheus.Labels{"type": "minecraft"}).Set(float64(minecraftConnections))
-}
-
-func UpdateModuleMetrics() {
-	CurrentTunnelPool.Lock()
-	defer CurrentTunnelPool.Unlock()
-
-	data := make(map[string]int)
-	for _, pair := range CurrentTunnelPool.pool {
-		if pair.Primary == nil {
-			continue
-		}
-
-		for _, module := range pair.Primary.ModuleHandler.GetModules() {
-			if module.IsEnabled() {
-				if _, ok := data[module.GetIdentifier()]; !ok {
-					data[module.GetIdentifier()] = 0
-				}
-
-				data[module.GetIdentifier()] += 1
-			}
-		}
-	}
-
-	metrics.UsedModules.Reset()
-	for module, count := range data {
-		metrics.UsedModules.With(prometheus.Labels{"identifier": module}).Set(float64(count))
-	}
 }
 
 func WrapConn(server, client *mcnet.Conn) *MinecraftTunnel {
