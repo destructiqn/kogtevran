a3610f1545d2712725f97df42bc6022c0659f34c
RuscalWorld <me@ruscalworld.ru>
2022-03-22 10:54:11 +0300

    Исправлено перетаскивание вкладок интерфейса

--- texteria/gui.js
+++ texteria/gui.js
@@ -1,8 +1,15 @@
 var Mouse = Java.type('org.lwjgl.input.Mouse')
 var TexteriaGui = Java.type('net.xtrafrancyz.mods.texteria.gui.TexteriaGui')
 
+var dragging = false
+
+function _click(x, y, button) {
+    dragging = button === 0 && self.hover
+}
+
 function _beforeRender() {
-    if (!Mouse.isButtonDown(0) || !self.hover) return
+    if (dragging && !self.hover) dragging = false
+    if (!Mouse.isButtonDown(0) || !dragging) return
     var scale = 1 / TexteriaGui.scaledResolution.e()
     self.x.set(self.x.render + Mouse.getDX() * scale)
     self.y.set(self.y.render - Mouse.getDY() * scale)
